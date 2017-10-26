a989e9c4d23ff71c8f11537ee7cd4542fd79bdd9
Michal Hanebach <michal.hanebach@gmail.com>
2017-10-26 20:56:46 +0200

    implement TodoList class module placeholder

---
+++ todolist.py
@@ -0,0 +1,21 @@
+from todoitem import *
+
+class TodoList():
+
+    def __init__(self):
+        pass
+
+    def add_item(self, name, description):
+        pass
+
+    def delete_item(self, id):
+        pass
+
+    def mark_item_as_done(self, id):
+        pass
+
+    def display_todolist(self):
+        pass
+
+    def display_todoitem_by_id(self, id):
+        pass
