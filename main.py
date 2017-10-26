7e256a76832470308996a1158abdda2a72e94857
Michal Hanebach <michal.hanebach@gmail.com>
2017-10-26 21:12:53 +0200

    implement main module placeholder

---
+++ main.py
@@ -0,0 +1,12 @@
+from todolist import *
+
+
+def main():
+    operations = ("Add todo item", "Modify existing item", "Delete item",
+                  "Mark item as done", "Display todo list", "Display item by id",
+                  "exit")
+    #todo: implement main
+
+
+if __name__ == "__main__":
+    main()
