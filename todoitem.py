c49ac410573124cd55c8e96c591d1e70d97fef46
Michal Hanebach <michal.hanebach@gmail.com>
2017-10-26 20:51:16 +0200

    implement TodoItem class module placeholder

---
+++ todoitem.py
@@ -0,0 +1,14 @@
+class TodoItem():
+
+    def __init__(self, name = "", description = "", is_done = False):
+        pass
+
+    def set_name(self, name):
+        pass
+
+    def set_desc(self, description):
+        pass
+
+    def set_done(status = True):
+        pass
+    
