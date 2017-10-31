0332d21aa6505680fc7d33ced86011219a2eb087
Michal Hanebach <michal.hanebach@gmail.com>
2017-10-31 01:43:40 +0100

    this SI assignment will suck, and I apologize for that. please take no offense

--- main.py
+++ main.py
@@ -1,11 +1,149 @@
 from todolist import *
 
 
+def print_menu(options):
+    print("\n Please choose an option by typing an ordinal.\n")
+    for i, opt in enumerate(options):
+        print(" " + str(i+1), opt)
+    print()
+
+
+def get_string_from_input(prompt, max_len):
+    in_str = ""
+    while not in_str or len(in_str) > max_len:
+        in_str = input(prompt)
+
+    return in_str
+
+
+def get_item_name_from_input():
+    return get_string_from_input(" Please provide task name (max. 20 chars): ", 20)
+
+
+def get_item_description_from_input():
+    return get_string_from_input(" Please provide task description (max. 150 chars): ", 150)
+
+
+def item_addition_handler(todolist):
+    name = get_item_name_from_input()
+    desc = get_item_description_from_input()
+    todolist.add_item(name, desc)
+    print(" Item added")
+
+
+def grab_todolist_item_index_from_input(todolist):
+    ordinal = None
+    while True:
+        try:
+            ordinal = int(input("\nChoose item id: "))
+        except ValueError:
+            print(" Invalid input")
+        else:
+            if ordinal - 1 not in range(0, len(todolist.items)):
+                print(" Input out of range")
+            else:
+                break
+    return ordinal - 1
+
+
+def item_deletion_handler(todolist):
+    if not todolist.items:
+        print(" The todo list is empty")
+        return
+
+    todolist.display_todolist()
+    index = grab_todolist_item_index_from_input(todolist)
+    todolist.delete_item(index)
+    print(" Deleted item")
+
+
+def item_modification_handler(todolist):
+    if not todolist.items:
+        print(" The todo list is empty")
+        return
+
+    todolist.display_todolist()
+    index = grab_todolist_item_index_from_input(todolist)
+    ans = ""
+    while True:
+        ans = input(" Write N for changing item name, D for changing its description: ")
+        if ans.lower() == "n" or ans.lower() == "d":
+            break
+
+    if ans.lower() == "n":
+        name = get_item_name_from_input()
+        todolist.items[index].set_name(name)
+        print(" Item name updated")
+    else:
+        desc = get_item_description_from_input()
+        todolist.items[index].set_desc(desc)
+        print(" Item description updated")
+
+
+def item_marking_handler(todolist):
+    if not todolist.items:
+        print(" The todo list is empty")
+        return
+
+    todolist.display_todolist()
+    index = grab_todolist_item_index_from_input(todolist)
+    todolist.mark_item_as_done(index)
+    print(" Marked item as done")
+
+
+def item_displaying_handler(todolist):
+    if not todolist.items:
+        print(" The todo list is empty")
+        return
+
+    todolist.display_todolist()
+    index = grab_todolist_item_index_from_input(todolist)
+    todolist.display_todoitem_by_id(index)
+
+
 def main():
     operations = ("Add todo item", "Modify existing item", "Delete item",
                   "Mark item as done", "Display todo list", "Display item by id",
                   "exit")
-    #todo: implement main
+
+    todolist = TodoList()
+
+    while True:
+        print_menu(operations)
+        # get option from input
+        ordinal = None
+        while True:
+            try:
+                ordinal = int(input("\nChoose operation: "))
+            except ValueError:
+                print(" Invalid input")
+            else:
+                if ordinal - 1 not in range(0, len(operations)):
+                    print(" Input out of range")
+                else:
+                    break
+        # examine input
+        index = ordinal - 1
+        if index == operations.index("Add todo item"):
+            item_addition_handler(todolist)
+
+        elif index == operations.index("Modify existing item"):
+            item_modification_handler(todolist)
+
+        elif index == operations.index("Delete item"):
+            item_deletion_handler(todolist)
+
+        elif index == operations.index("Mark item as done"):
+            item_marking_handler(todolist)
+
+        elif index == operations.index("Display todo list"):
+            todolist.display_todolist()
+
+        elif index == operations.index("Display item by id"):
+            item_displaying_handler(todolist)
+
+        elif index == operations.index("exit"):
+            break
 
 
 if __name__ == "__main__":
--- todolist.py
+++ todolist.py
@@ -8,15 +8,16 @@ class TodoList():
     def add_item(self, name, description):
         self.items.append(TodoItem(name, description))
 
-    def delete_item(self, identifier):
-        del self.items[identifier - 1]
+    def delete_item(self, idx):
+        del self.items[idx]
 
-    def mark_item_as_done(self, identifier):
-        self.items[identifier - 1].set_done()
+    def mark_item_as_done(self, idx):
+        self.items[idx].is_done = True
 
     def display_todolist(self):
-        for i in range(1,len(self.items) + 1):
+        for i in range(0,len(self.items)):
             self.display_todoitem_by_id(i)
 
-    def display_todoitem_by_id(self, identifier):
-        print(" %u  %s" % (identifier, self.items[identifier - 1].name))
+    def display_todoitem_by_id(self, idx):
+        done = "done" if self.items[idx].is_done else "not done"
+        print(" %u  %s, %s (%s)" % (idx + 1, self.items[idx].name, self.items[idx].description, done))
